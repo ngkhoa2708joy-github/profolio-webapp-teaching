0b213ad32c253b38c78eb8f2420c4d615e3c799c
Khoa Nguyen <ngkhoa2708.joy@gmail.com>
2026-09-16 13:17:11 +0700

    debug update UI and Icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,11 @@ export const metadata: Metadata = {
   description: "Learning hub for schedules, students, and assignments.", // Mô tả web (dùng cho SEO)
 };
 
-export default function RootLayout({ children }: LayoutProps<"/">) {
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html
       lang="en"
